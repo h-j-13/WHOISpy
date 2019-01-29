10e542e6185becddfad5c61cc912ca65921a361c
HouJie <h.j.13.new@gmail.com>
2019-01-29 20:32:58 +0800

    add : WHOIS info extract func init & add standard WHOIS record dict

---
+++ WHOISpy/WHOIS_info_extract.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+"""
+域名WHOIS数据抽取
+
+"""
+
+domain_whois = {
+    "domain": "",  # 域名
+    "tld": "",  # 顶级域
+    "flag": "",  # 状态标记
+    "domain_status": "",  # 域名状态
+    "sponsoring_registrar": "",  # 注册商
+    "top_whois_server": "",  # 顶级域名服务器
+    "sec_whois_server": "",  # 二级域名服务器
+    "reg_name": "",  # 注册姓名
+    "reg_phone": "",  # 注册电话
+    "reg_email": "",  # 注册email
+    "org_name": "",  # 注册公司名称
+    "creation_date": "",  # 创建时间
+    "expiration_date": "",  # 到期时间
+    "updated_date": "",  # 更新时间
+    "details": "",  # 原始信息
+    "name_server": "",  # 域名服务器
+}
